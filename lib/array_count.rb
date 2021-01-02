31325eaaee00c908378bf9ff7f653e70a4bd83c9
Blaine R Love <blainelove94@gmail.com>
2021-01-02 18:58:32 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -8,8 +8,6 @@ end
 def count_empty_strings(array)
   array.count do |string|
     string.is_a? String and == ""
-
-    end
   end
 
   # Return the total number of EMPTY strings in the provided array using the count enumerable
